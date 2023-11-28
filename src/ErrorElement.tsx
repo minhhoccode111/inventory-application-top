d9757df4edafd6ab30a64b0cbba80d37a845e0e5
Đặng Hoàng Minh <minhhoccode111@gmail.com>
2023-11-28 18:13:14 +0700

    Added navigate button using useNavigate hook

--- src/ErrorElement.tsx
+++ src/ErrorElement.tsx
@@ -1,6 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
   return (
     <div>
       <h1 className="text-8xl">404</h1>
@@ -8,6 +13,9 @@ const ErrorPage = () => {
         Page not found you son of a stupid bitch, what are you trying to do?
       </p>
       <Link to="/">Go fucking back right now!</Link>
+      <button className="text-blue text-2xl cursor-pointer" onClick={goBack}>
+        Go back
+      </button>
     </div>
   );
 };
