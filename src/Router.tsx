e6041d610c7d0f7f5de7494151a9a410ccdcd67f
Đặng Hoàng Minh <minhhoccode111@gmail.com>
2023-11-30 21:33:10 +0700

    clean up and do tutorial

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-import Link0 from "./Link0";
-import Link1 from "./Link1";
-import Link2 from "./Link2";
-import Link3 from "./Link3";
 import ErrorElement from "./ErrorElement";
-import DefaultElement from "./DefaultElement";
 
 const Router = () => {
   const router = createBrowserRouter([
@@ -13,28 +8,6 @@ const Router = () => {
       path: "/",
       element: <App />,
       errorElement: <ErrorElement />,
-      children: [
-        {
-          index: true,
-          element: <DefaultElement />,
-        },
-        {
-          path: "link0",
-          element: <Link0 />,
-        },
-        {
-          path: "link1",
-          element: <Link1 />,
-        },
-        {
-          path: "link2",
-          element: <Link2 />,
-        },
-        {
-          path: "link3",
-          element: <Link3 />,
-        },
-      ],
     },
   ]);
   return <RouterProvider router={router} />;
