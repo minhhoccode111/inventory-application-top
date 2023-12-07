a0a090971cfddb71014a92fe43afa870f5cd66d6
Đặng Hoàng Minh <minhhoccode111@gmail.com>
2023-12-07 11:30:20 +0700

    understand contact element

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -1,6 +1,8 @@
 import { useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
+  // this error page is used in errorElement property
+  // and this hook returns anything thrown during an action, loader, or rendering
   const error = useRouteError();
   console.log(error);
   return (
@@ -8,6 +10,7 @@ const ErrorPage = () => {
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
+        {/* we display the statusText or message of the error */}
         <i>{error.statusText || error.message}</i>
       </p>
     </div>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,7 @@
+// this component is a default element of dynamic segment in root
 export default function Index() {
   return (
+    // which will show a greeting or more info section
     <p id="zero-state">
       This is a demo for React Router.
       <br />
