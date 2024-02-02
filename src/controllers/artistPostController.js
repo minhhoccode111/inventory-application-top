619cab3a4e5697e13719d6bb2283139944401195
minhhoccode111 <minhhoccode111@gmail.com>
2024-02-02 17:29:55 +0700

    handle /artist/:id/update post request which update new image when the artist doesnt have image before, but a bug out of sync occur when artist already has an image and then both images get wipe out

--- src/controllers/artistPostController.js
+++ src/controllers/artistPostController.js
@@ -1,6 +1,8 @@
 const { body, validationResult } = require('express-validator');
 const asyncHandler = require('express-async-handler');
 const print = require('debug')('artist-post-debug');
+
+// work with files
 const fs = require('fs');
 const path = require('path');
 const extra = require('fs-extra');
@@ -18,9 +20,9 @@ const storage = multer.diskStorage({
 const upload = multer({ storage, limits }).single('avatar');
 const uploadWrapper = (req, res, next) => {
   upload(req, res, (err) => {
-    if (err) req.hasError = true; // file + error
-    else if (req.file) req.hasError = false; // file + 0 error
-    // else req.hasError = undefined // 0 file + 0 error
+    if (err) req.hasError = true; // => file + error
+    else if (req.file) req.hasError = false; // => file + 0 error
+    // else req.hasError = undefined // => 0 file + 0 error
     next();
   });
 };
@@ -98,12 +100,111 @@ module.exports.artist_create_post = [
 ];
 
 module.exports.artist_update_post = [
-  //
-  body(),
-  body(),
-  body(),
-  body(),
-  body(),
-  body(),
-  asyncHandler(async (req, res, next) => {}),
+  uploadWrapper,
+  body('name', 'Name field cannot be empty.').trim().isLength({ min: 1 }).escape(),
+  body('description', 'Description field cannot be empty.').trim().isLength({ min: 1 }).escape(),
+  body('added_by', 'Added by field cannot be empty.').trim().isLength({ min: 1 }).escape(),
+  body('personal_rating')
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage('Personal rating field cannot be empty.')
+    .custom((value) => !isNaN(Number(value)) && Number(value) >= 0 && Number(value) <= 10)
+    .withMessage('Personal rating value must between 0 and 10.')
+    .escape()
+    // validate file uploaded here to get proper error message
+    .custom((value, { req }) => !req.hasError)
+    .withMessage(`That's file is too large (>4MB)!`),
+  asyncHandler(async (req, res, next) => {
+    const error = validationResult(req);
+
+    const oldArtist = await Artist.findById(req.params.id).exec();
+
+    // no artist with req.params.id
+    if (oldArtist === null) {
+      const err = new Error('Artist not found');
+      err.status = 404;
+      next(err);
+    }
+
+    const newArtist = new Artist({
+      name: req.body.name,
+      created_at: oldArtist.created_at, // keep
+      last_modified: Date.now(),
+      added_by: req.body.added_by,
+      description: req.body.description,
+      personal_rating: req.body.personal_rating,
+      // case no file update or update the same file extension
+      extension: oldArtist.extension, // keep
+      _id: oldArtist._id, // keep
+    });
+
+    // all valid
+    if (error.isEmpty()) {
+      print('all valid');
+      // has file, save file information
+      if (req.hasError === false) {
+        print(`has file, save file information`, req.file, req.body);
+        const newExtension = req.file.originalname.split('.')[1];
+        const oldExtension = oldArtist.extension; // null?
+        newArtist.extension = newExtension; // update extension
+
+        // delete old file manually
+        if (newExtension !== oldExtension && oldExtension !== null) {
+          print(`have to delete old file manually`);
+          const oldFilename = oldArtist._id + '.' + oldExtension;
+          const uploadsDir = path.join(__dirname, `../../public/images/uploads`);
+          fs.readdir(uploadsDir, (err, files) => {
+            print(`read in dir: `, uploadsDir);
+            if (err) throw err;
+            files.forEach((file) => {
+              const filePath = path.join(uploadsDir, file);
+              if (file === oldFilename) {
+                print(`found the file to delete: `, file);
+                fs.unlink(filePath, (err) => {
+                  if (err) throw err;
+                  print(`successfully remove file: ${file}`);
+                });
+              }
+            });
+          });
+        } else {
+          // override old file anyway (whether null or not)
+          print(`override old file anyway (whether null or not)`);
+          const newFilename = oldArtist._id + '.' + newExtension;
+          const src = path.join(__dirname, `../../public/images/tmp/${req.file.filename}`);
+          const des = path.join(__dirname, `../../public/images/uploads/${newFilename}`);
+          extra.move(src, des, { overwrite: true }, (err) => {
+            if (err) throw err;
+            print(`successfully move file from ${src} to ${des}`);
+          });
+        }
+      }
+      print(`newly updated artist: `, newArtist);
+
+      await Artist.findByIdAndUpdate(req.params.id, newArtist);
+      res.redirect(`/music/artist/${req.params.id}`);
+    }
+
+    // invalid
+
+    // a file uploaded
+    if (req.hasError === false) {
+      print(`data is invalid and a file is uploaded: `, req.body, req.file);
+      const dir = path.join(__dirname, `../../public/images/tmp`);
+      // wipe whole tmp dir
+      fs.rm(dir, { recursive: true, force: true }, (err) => {
+        if (err) throw err;
+        print(`${dir} is deleted!`);
+      });
+    }
+
+    print(`artist and errors send back to form: `, newArtist, error.array());
+
+    // render form again
+    res.render('artist_form', {
+      title: 'Create Artist',
+      artist: newArtist,
+      errors: error.array(),
+    });
+  }),
 ];
